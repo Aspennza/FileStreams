8d60731d5dc5cb0c878f47b8217117a795bb6858
Zoe Aspenns <aspennza@mail.uc.edu>
2025-11-15 17:33:49 -0500

    Created the generateFrame method

--- src/RandProductMaker.java
+++ src/RandProductMaker.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -7,6 +8,10 @@ import java.util.ArrayList;
 public class RandProductMaker
 {
     private JFrame frame;
+    private TitlePnl titlePnl;
+    private ProductDataPnl productDataPnl;
+    private ControlPnl controlPnl;
+    private CounterPnl counterPnl;
     private int filesSaved;
     private ArrayList<Product> products;
     private File workingDirectory;
@@ -24,5 +29,81 @@ public class RandProductMaker
     private Path nameFile()
     {
         return Paths.get(workingDirectory.getPath() + "\\src\\RandProductData" + (filesSaved + 1) + ".bin");
+    }
+
+    public void generateFrame() {
+        frame = new JFrame();
+
+        //GridBagConstraints for the titlePnl
+        GridBagConstraints gbc1 = new GridBagConstraints();
+        gbc1.gridx = 0;
+        gbc1.gridy = 0;
+        gbc1.gridwidth = 1;
+        gbc1.gridheight = 1;
+        gbc1.weightx = 1;
+        gbc1.fill = GridBagConstraints.BOTH;
+
+        //GridBagConstraints for the productDataPnl
+        GridBagConstraints gbc2 = new GridBagConstraints();
+        gbc2.gridx = 0;
+        gbc2.gridy = 1;
+        gbc2.gridwidth = 1;
+        gbc2.gridheight = 3;
+        gbc2.weightx = 1;
+        gbc2.fill = GridBagConstraints.BOTH;
+
+        //GridBagConstraints for the controlPnl
+        GridBagConstraints gbc3 = new GridBagConstraints();
+        gbc3.gridx = 0;
+        gbc3.gridy = 4;
+        gbc3.gridwidth = 1;
+        gbc3.gridheight = 1;
+        gbc3.weightx = 1;
+        gbc3.weighty = 1;
+        gbc3.fill = GridBagConstraints.BOTH;
+
+        //GridBagConstraints for the counterPnl
+        GridBagConstraints gbc4 = new GridBagConstraints();
+        gbc4.gridx = 0;
+        gbc4.gridy = 5;
+        gbc4.gridwidth = 1;
+        gbc4.gridheight = 1;
+        gbc4.weightx = 1;
+        gbc4.fill = GridBagConstraints.BOTH;
+
+        JPanel mainPnl = new JPanel();
+
+        //This Toolkit is used to find the screen size of the computer running the GUI
+        Toolkit kit = Toolkit.getDefaultToolkit();
+
+        //This Dimension stores the screen size
+        Dimension screenSize = kit.getScreenSize();
+
+        //This int stores the height of the screen
+        int screenHeight = screenSize.height;
+
+        //This int stores the width of the screen
+        int screenWidth = screenSize.width;
+
+        mainPnl.setLayout(new GridBagLayout());
+        frame.add(mainPnl);
+
+        titlePnl = new TitlePnl();
+        mainPnl.add(titlePnl, gbc1);
+
+        productDataPnl = new ProductDataPnl();
+        mainPnl.add(productDataPnl, gbc2);
+
+        controlPnl = new ControlPnl();
+        mainPnl.add(controlPnl, gbc3);
+
+        counterPnl = new CounterPnl();
+        mainPnl.add(counterPnl, gbc4);
+
+        frame.setSize(screenWidth * 3 / 4, screenHeight * 3 / 4);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Product Maker");
+        frame.setVisible(true);
     }
 }
