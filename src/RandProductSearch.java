570e6b283dcd3da20cc3829f4a6dc96c96bc1dfb
Zoe Aspenns <aspennza@mail.uc.edu>
2025-11-17 12:20:26 -0500

    Returned the program to its original state

--- src/RandProductSearch.java
+++ src/RandProductSearch.java
@@ -6,9 +6,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-
-
-
 public class RandProductSearch
 {
     private JFrame frame;
@@ -42,9 +39,107 @@ public class RandProductSearch
         }
     }
 
+    private static Product readProductData(RandomAccessFile randFile, long position)
+    {
+        try {
+            if (position >= randFile.length()) {
+                return null; //This means there are no more records in the file
+            }
+
+            randFile.seek(position);
+            byte[] IDBytes = new byte[6];
+            randFile.readFully(IDBytes);
+            String ID = new String(IDBytes, StandardCharsets.UTF_8).trim();
+
+            byte[] nameBytes = new byte[35];
+            randFile.readFully(nameBytes);
+            String name = new String(nameBytes, StandardCharsets.UTF_8).trim();
+
+            byte[] descripBytes = new byte[75];
+            randFile.readFully(descripBytes);
+            String description = new String(descripBytes, StandardCharsets.UTF_8).trim();
+
+            double cost = randFile.readDouble();
 
 
+            Product product = new Product(ID, name, description, cost);
+            return product;
+        } catch (IOException e) {
+        e.printStackTrace();
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Error reading product data:\n" + e.getMessage(),
+                    "File Read Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        return null; //This means there was a corrupted record
+        }
+    }
 
+    public void loadAllProducts() {
+        products.clear();
+        recordsRead = 0;
+
+        if(file == null) {
+            JOptionPane.showMessageDialog(null, "No file was selected.");
+            return;
+        }
+
+        try(RandomAccessFile randFile = new RandomAccessFile(file.toFile(), "r")) {
+            Product p;
+            while ((p = readProductData(randFile, recordsRead * RECORD_SIZE)) != null) {
+                products.add(p);
+                recordsRead++;
+            }
+
+        if(products.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "This file does not appear to contain any products.");
+        } else {
+            JOptionPane.showMessageDialog(null, products.size() + " products were loaded from the file.");
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Error reading product data:\n" + e.getMessage(),
+                    "File Read Error",
+                    JOptionPane.ERROR_MESSAGE
+            );
+        }
+    }
+
+    public void searchFile()
+    {
+        if (products.isEmpty())
+        {
+            JOptionPane.showMessageDialog(null, "You must select a non-empty file before searching.");
+            return;
+        }
+
+        if(fileSearchPnl.getSearchStringTF().getText().trim().isEmpty())
+        {
+            JOptionPane.showMessageDialog(null, "You must enter a search string before searching the file.");
+            return;
+        }
+
+        if(!filteredProductsPnl.getFilteredProductsTA().getText().isEmpty())
+        {
+            filteredProductsPnl.getFilteredProductsTA().setText("");
+        }
+
+        int productIndex = 1;
+
+        for (Product p : products)
+        {
+            if(p.getName().toLowerCase().contains(fileSearchPnl.getSearchStringTF().getText().trim().toLowerCase()))
+            {
+                filteredProductsPnl.getFilteredProductsTA().append("Product " + productIndex + "\nID: " + p.getID() + "\nName: " + p.getName() + "\nDescription: " + p.getDescription() + "\nCost: $" +  String.format("%.2f", p.getCost()) + "\n-----------------------------\n");
+                productIndex++;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "Search complete.");
+    }
 
     public void resetProgram()
     {
